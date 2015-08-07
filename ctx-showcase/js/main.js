0a2d7ad7fed18b518fd2b5ec89c36d3396d1d7a8
Momchil Gorchev <momchil.gorchev@gmail.com>
2015-08-07 17:09:48 +0100

    Nav updated

--- ctx-showcase/js/main.js
+++ ctx-showcase/js/main.js
@@ -2,4 +2,14 @@
 (function(){
     'use strict';
     console.log('Hello world!');
+
+    $('#dropdown-button').dropdown({
+        inDuration: 300,
+        outDuration: 225,
+        constrain_width: false, // Does not change width of dropdown to that of the activator
+        hover: true, // Activate on hover
+        gutter: 0, // Spacing from edge
+        belowOrigin: true // Displays dropdown below the button
+    });
+
 })();
